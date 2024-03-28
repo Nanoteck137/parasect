7873fb37de1e4f9885ed77f4bbf8beecfc924fb0
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-03-28 12:25:25 +0100

    Added CopyFile util

--- parasect.go
+++ parasect.go
@@ -204,3 +204,28 @@ func IsValidExt(exts []string, ext string) bool {
 
 	return false
 }
+
+func CopyFile(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
+}
